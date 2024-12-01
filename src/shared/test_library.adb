8f6a0b8dff031bada0a37a9ec35caa865f5b80fc
Matthew Sessions <mbrittsess@hotmail.com>
2024-11-30 23:10:39 -0500

    First commit

---
+++ src/shared/test_library.adb
@@ -0,0 +1,6 @@
+package body Test_Library is
+   function Test_Function return String is
+   begin
+      return "Hello, world!";
+   end Test_Function;
+end Test_Library;
---
+++ src/shared/test_library.ads
@@ -0,0 +1,3 @@
+package Test_Library is
+   function Test_Function return String;
+end Test_Library;
