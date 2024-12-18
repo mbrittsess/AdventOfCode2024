f1a301b53bb3e4823713a2c2552c4befa3e5d258
Matthew Sessions <msessions@beckettcorp.com>
2024-12-18 15:44:04 -0500

    Implemented input processing for day 9 common parent package

---
+++ src/day9/day9-solution.adb
@@ -0,0 +1,8 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Day9.Solution is
+begin
+   Put_Line("Input is " & Input_Blocks'Length'Image & " blocks long");
+
+   Put_Line("Disk uses " & Disk_Size'Image & " bytes");
+end Day9.Solution;
---
+++ src/day9/day9.ads
@@ -0,0 +1,30 @@
+with Ada.Text_IO;
+
+package Day9 is
+   type Storage_Kind is (File, Empty);
+
+   -- The default value for Kind is required for us to be able to have an array of mixed Block types
+   type Block (Kind : Storage_Kind := Empty) is
+      record
+         Size : Natural;
+         case Kind is
+            when File =>
+               Index : Natural;
+            when others =>
+               null;
+         end case;
+      end record;
+   
+   Input_Line : constant String := Ada.Text_IO.Get_Line;
+
+   Raw_Input_Blocks : constant array (Positive range <>) of Block :=
+      [for Idx in Input_Line'Range =>
+         (if Idx mod 2 = 1 then
+            (Kind => File, Size => Character'Pos(Input_Line(Idx)) - Character'Pos('0'), Index => (Idx-1)/2)
+          else
+            (Kind => Empty, Size => Character'Pos(Input_Line(Idx)) - Character'Pos('0')))];
+   
+   Input_Blocks : constant array (Positive range <>) of Block := [for Bl of Raw_Input_Blocks when Bl.Size /= 0 => Bl];
+   
+   Disk_Size : constant Positive := [for Bl of Input_Blocks => Bl.Size]'Reduce("+", Natural'(0));
+end Day9;
